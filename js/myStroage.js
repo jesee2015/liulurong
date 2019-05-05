4a276a1c5044b458af0254d398bbb335770ec18c
liuxi <shakelx@qq.com>
2019-05-05 21:06:59 +0800

    增加设置

--- js/myStroage.js
+++ js/myStroage.js
@@ -13,11 +13,21 @@ function initDate() {
 		var strBills = JSON.stringify(Bills);
 		plus.storage.setItem('bills', strBills);
 	}
+	if (plus.storage.getItem('markets') == null || plus.storage.getItem('markets') == undefined) {
+		var Markets = [];
+		var strMarkets = JSON.stringify(Markets);
+		plus.storage.setItem('markets', strMarkets);
+	}
 }
 
 function getBills() {
 	var strBills = plus.storage.getItem('bills');
 	return JSON.parse(strBills);
+}
+
+function getMarkets() {
+	var strMarkets = plus.storage.getItem('markets');
+	return JSON.parse(strMarkets);
 }
 
 function getBillsByDate(date) {
@@ -36,14 +46,27 @@ function SaveBillDate(objBillData) {
 	plus.storage.setItem('bills', strBills);
 }
 
+function saveMarketData(objMarketData) {
+	var markets = getMarkets();
+	markets.push(objMarketData);
+	var strMarkets = JSON.stringify(markets);
+	plus.storage.setItem('markets', strMarkets);
+}
+
 function removeBillDate(objBillData) {
-	// console.log('form myStroage file:' + JSON.stringify(objBillData));
 	var billDatas = getBills();
 	var index = billDatas.indexOf(objBillData);
 	billDatas.splice(index, 1);
 	var newStrBillsData = JSON.stringify(billDatas);
-	// console.log(newStrBillsData);
 	plus.storage.removeItem('bills');
 	plus.storage.setItem('bills', newStrBillsData);
+}
 
+function removeMarketDate(objMarketData) {
+	var marketDatas = getMarkets();
+	var index = marketDatas.indexOf(objMarketData);
+	marketDatas.splice(index, 1);
+	var newStrMarketsData = JSON.stringify(marketDatas);
+	plus.storage.removeItem('markets');
+	plus.storage.setItem('markets', newStrBillsData);
 }
